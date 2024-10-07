9d38b352f8a7e4438bcc853531c7b28a0ab359ca
Karani Wachira <wachirakeith@gmail.com>
2024-10-07 14:11:47 +0300

    feat: added the comment form into the single post component

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {PostCardComponent} from "../../layouts/post-card/post-card.component";
+import {CommentFormComponent} from "../../comments/comment-form/comment-form.component";
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
   imports: [
-    PostCardComponent
+    PostCardComponent,
+    CommentFormComponent
   ],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
